a77832fd83810ba3552e1822ecec68861c05d8d0
LazyTexas <3165851319@qq.com>
2026-07-02 16:05:10 +0800

    Revert "Merge pull request #6 from LazyTexas/feature/backend-framework"
    This reverts commit d65be98d284512753ca13b860210c9a39b80ef37, reversing
    changes made to cb9396d332cc904e71e9b6bab6d951363291474c.

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///../data/decisionjury.db")
-    DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY", "")
